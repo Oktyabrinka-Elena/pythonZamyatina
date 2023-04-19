8bab73df06cd44401b5799293a348cb21c2c2d61
ElenaZamyatina <elenzamyatina12@gmail.com>
2023-04-19 23:25:33 +0400

    38 create

--- DZ8/script.py
+++ DZ8/script.py
@@ -1,13 +1,30 @@
 # основной код
+from data_create import name_data, surname_data, phone_data, address_data
+
 
 def input_data():
-    pass
+    name = name_data()
+    surname = surname_data()
+    phone = phone_data()
+    address = address_data()
 
-def print_data():
-    pass
+    var = int(input(f"В каком формате Вы хотите записать данные?\n\n"
+                    f"1 Вариант:\n\n"
+                    f"{surname}\n"
+                    f"{name}\n"
+                    f"{phone}\n"
+                    f"{address}\n\n"
+                    f"2 Вариант:\n\n"
+                    f"{surname};{name};{phone};{address}\n\n"
+                    f"Выберите номер варианта: "))
 
-def delete_data():
-    pass
+    while var != 1 and var != 2:
+        print('Ты дурак?! Даю тебе последний шанс')
+        var = int(input("Введите номер варианта: "))
 
-def put_data():
-    pass
+    if var == 1:
+        with open('data_first_variant.csv', 'a', encoding='utf-8') as file:
+            file.write(f'{name}\n{surname}\n{phone}\n{address}\n\n')
+    else:
+        with open('data_second_variant.csv', 'a', encoding='utf-8') as file:
+            file.write(f'{name};{surname};{phone};{address}')
